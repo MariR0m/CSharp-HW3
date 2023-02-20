12a74688c793c313337ae43a6890ba8d08b1de91
Mari <r.m.u@bk.ru>
2023-02-20 15:39:04 +0300

    Сделано ДЗ к 6 семинару

---
+++ HW6-1/HW6-1.cs
@@ -0,0 +1,94 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int [] EnterNumbers (int size, int minValue, int maxValue)
+// {
+//     int [] numbers = new int [size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         System.Console.WriteLine("Введите число от " + minValue + " до " + maxValue);
+//        numbers [i] = Convert.ToInt32(Console.ReadLine());
+//     }
+// return numbers;
+// }
+// Вариант 1: 
+
+// int[] EnterNumbers(int size, int minValue, int maxValue)
+// {
+//     int[] numbers = new int[size];
+//     int number;
+//     for (int i = 0; i < size; i++)
+//     {
+//         System.Console.WriteLine("Введите число от " + minValue + " до " + maxValue);
+//         number = Convert.ToInt32(Console.ReadLine());
+//         if (number > minValue && number < maxValue)
+//         {
+//             numbers[i] = +number;
+//         }
+//         else System.Console.WriteLine("Вы ввели не число");
+//     }
+//     return numbers;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     System.Console.WriteLine("[" + string.Join("; ", array) + "]");
+// }
+
+// int PositiveNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// int[] MyArray = EnterNumbers(5, -100, 100);
+// PrintArray(MyArray);
+// int QuantityPositiveNumbers = PositiveNumbers(MyArray);
+
+// System.Console.WriteLine("Введенных чисел болольше 0 : " + QuantityPositiveNumbers);
+
+int[] EnterNumbers(int size)
+{
+    int[] numbers = new int[size];
+    
+     for (int i = 0; i < size; i++)
+     {
+         System.Console.WriteLine("Введите число: ");
+        numbers [i] = Convert.ToInt32(Console.ReadLine());
+     }
+ return numbers;
+ }
+
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join("; ", array) + "]");
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] MyArray = EnterNumbers(5);
+PrintArray(MyArray);
+int QuantityPositiveNumbers = PositiveNumbers(MyArray);
+
+System.Console.WriteLine("Введенных чисел болольше 0 : " + QuantityPositiveNumbers);
---
+++ HW6-2/HW6-2.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double InputNamber(string str)
+{
+    System.Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double CalculationX (double b1, double b2, double k1, double k2)
+{
+double x;
+x = (b2-b1)/(k1-k2);
+return x;
+}
+
+double CalculationY ( double b2, double k2, double x)
+{
+double y;
+y= k2*x+b2;
+return y;
+}
+
+double b1 = InputNamber("Введите координату b1: ");
+double k1 = InputNamber("Введите координату k1: ");
+double b2 = InputNamber("Введите координату b2: ");
+double k2 = InputNamber("Введите координату k2: ");
+
+double x = CalculationX(b1, b2, k1, k2);
+double y = CalculationY( b2, k2, x);
+
+System.Console.WriteLine( x + ", " + y);
