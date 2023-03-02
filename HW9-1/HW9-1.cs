a0df3bd219d64bc1494993a1534d24e4c8072109
Mari <r.m.u@bk.ru>
2023-03-02 16:49:11 +0300

    Сделала ДЗ к Семинару 9

---
+++ HW9-1/HW9-1.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Output(int M, int N)
+{
+    if(N < M)
+    return 0;
+    return N+Output(M, N - 1);
+}
+
+int M = ReadInt("Введите число M:");
+int N = ReadInt("Введите число N:");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine(Output(M,N));
---
+++ HW9-2/HW9-2.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Output(int M, int N)
+{
+    if (M == 0)
+        return (N + 1);
+
+    else if (M > 0 && N == 0)
+        return Output((M - 1), 1);
+
+    else if (M > 0 && N > 0)
+        return Output((M - 1), Output(M, (N - 1)));
+
+    return 0;
+}
+
+M:
+int M = ReadInt("Введите число M:");
+N:
+int N = ReadInt("Введите число N:");
+
+if (M < 0)
+{
+    System.Console.WriteLine("Введено число M меньше 0, введите верное число");
+    goto M;
+}
+else if (N < 0)
+{
+    System.Console.WriteLine("Введено число N меньше 0, введите верное число");
+    goto N;
+}
+
+System.Console.WriteLine();
+
+System.Console.WriteLine(Output(M, N));
